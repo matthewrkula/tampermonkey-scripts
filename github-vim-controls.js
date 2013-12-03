fc08e059ebaaf7e4b14bf6e9fba2abd89dff7559
Valentin Litvak <contact@vallitvak.com>
2013-12-03 00:43:36 -0600

    adjusted the code to standards

--- github-vim-controls.js
+++ github-vim-controls.js
@@ -8,55 +8,54 @@
 // @copyright  2012+, You
 // ==/UserScript==
 
-
 var i = 0;
-var files = $(".content");
+var files = $('.content');
 var stack = [];
 
 changeOpacity();
 
 function reset(){
-    setTimeout(function(){
-        files = $('.content')
-        changeOpacity();
-    }, 500);
+  setTimeout(function() {
+    files = $('.content')
+    changeOpacity();
+  }, 500);
 }
 
-function forward(){
-    stack.push(i);
+function forward() {
+  stack.push(i);
+  i = 0;
+  reset();
+}
+
+function backward() {
+  if(stack.length > 0)
+    i = stack.pop();
+  else 
     i = 0;
-    reset();
+  reset();
 }
 
-function backward(){
-    if(stack.length > 0)
-    	i = stack.pop();
-    else 
-        i = 0;
-    reset();
-}
-
-function changeOpacity(){
-    files.css('opacity', '1');
-    $(files[i]).css('opacity', '0.5');
+function changeOpacity() {
+  files.css('opacity', '1');
+  $(files[i]).css('opacity', '0.5');
 }
 
 window.onpopstate = backward;
 
-$(document).keypress(function(event){
-    if(event.which == 106){   
-        i += 1;
-        if(i == files.length)
-            i -= 1;
-        changeOpacity();  
-    }else if(event.which == 107){   
-        i -= 1;
-        if(i < 0)
-            i = 0;
-        changeOpacity();
-    }else if(event.which == 13){
-        $(files[i]).find('a').click();
-        forward();     
-    }      
+$(document).keypress(function(event) {
+  if(event.which == 106) {
+    i += 1;
+    if(i == files.length)
+      i -= 1;
+    changeOpacity();
+  } else if(event.which === 107) {
+      i -= 1;
+      if(i < 0)
+        i = 0;
+      changeOpacity();
+  } else if (event.which === 13) {
+    $(files[i]).find('a').click();
+    forward();
+  }
 });
 
