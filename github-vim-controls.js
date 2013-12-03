fbd72d17a68e94089887ac489f94d50ea1c83167
Matt Kula <matthewrkula@gmail.com>
2013-12-03 00:43:59 -0600

    Updating the spacing for Val :)

--- github-vim-controls.js
+++ github-vim-controls.js
@@ -15,47 +15,47 @@ var stack = [];
 changeOpacity();
 
 function reset(){
-    setTimeout(function(){
-        files = $('.content')
-        changeOpacity();
-    }, 500);
+  setTimeout(function(){
+    files = $('.content')
+    changeOpacity();
+  }, 500);
 }
 
 function forward(){
-    stack.push(i);
-    i = 0;
-    reset();
+  stack.push(i);
+  i = 0;
+  reset();
 }
 
 function backward(){
-    if(stack.length > 0)
-    	i = stack.pop();
-    else 
-        i = 0;
-    reset();
+  if(stack.length > 0)
+   i = stack.pop();
+  else 
+   i = 0;
+  reset();
 }
 
 function changeOpacity(){
-    files.css('opacity', '1');
-    $(files[i]).css('opacity', '0.5');
+  files.css('opacity', '1');
+  $(files[i]).css('opacity', '0.5');
 }
 
 window.onpopstate = backward;
 
 $(document).keypress(function(event){
-    if(event.which == 106){   
-        i += 1;
-        if(i >= files.length)
-            i -= 1;
-        changeOpacity();  
-    }else if(event.which == 107){   
-        i -= 1;
-        if(i < 0)
-            i = 0;
-        changeOpacity();
-    }else if(event.which == 13){
-        $(files[i]).find('a').click();
-        forward();     
-    }      
+  if(event.which == 106){   
+    i += 1;
+    if(i >= files.length)
+      i -= 1;
+    changeOpacity();  
+  }else if(event.which == 107){   
+    i -= 1;
+    if(i < 0)
+      i = 0;
+    changeOpacity();
+  }else if(event.which == 13){
+    $(files[i]).find('a').click();
+    forward();     
+  }      
 });
 
